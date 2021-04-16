b93631602a1f8ed57df35e33bf0b01237611a6a5
Essi Suomalainen <essi.suomalainen@hyperisland.se>
2021-04-16 14:30:22 +0200

    basic structure and added api information

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
+const api = {
+  key: '1aeef9314e710b0d8aa7bf74261257ea',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
